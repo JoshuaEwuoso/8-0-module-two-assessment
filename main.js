201da3a425520e810ecc8c29ca18833901d6e245
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-10-14 16:21:49 -0400

    completes main functinality

--- main.js
+++ main.js
@@ -1,61 +1,47 @@
+const select = document.querySelector("select");
 const Base_URL = "https://ghibliapi.herokuapp.com/films"
 
 fetch(Base_URL)
 .then((rev) => {
     return rev.json()
 })
-.then((movies) => {
-    // const body = document.querySelector("body");
-    // const select = document.createElement("select");
-    // select.setAttribute("value", "default")
-    // body.append(select)
     
-    // section = document.createElement('section');
-    // section.setAttribute("id", "display-info")
-    // select.append(section)
-    // const form = document.createElement("form");
-    // const label = document.createElement("label");
-    // const input = document.createElement("input");
-    // const input2 = document.createElement("input")
-    // section.append(form)
-    // form.append(label)
-    // input.setAttribute("type", "text")
-    // label.append(input)
-    // input2.setAttribute("type", "submit")
-    // input2.setAttribute("value", "Submit Review")
-    // form.append(input2)
-    // const ul = document.createElement("ul")
-    // form.append(ul)
+.then((movies) => {
     let select = document.querySelector("select");
+    const form = document.querySelector("form")
+    let input = document.querySelector("#input");
 
-    for (let movie of movies) {
+    movies.forEach((movie) => {
         const option = document.createElement("option");
         option.setAttribute("value", movie.title)
         option.textContent = movie.title
         select.append(option)
-
-    }
-
-        select.addEventListener("change", () => {
-            for (let movie of movies) {
-                if(select.value === movie.title) {
-                    const h3 = document.createElement("h3")
-                    h3.textContent = movie.title
-                    const pDate = document.createElement("p")
-                    pDate.textContent = movie.release_date
-                    const pSummary = document.createElement("p")
-                    pSummary.textContent = movie.description
-                    const section = document.querySelector("section");
-                    section.prepend(h3)
-                    h3.append(pDate)
-                    h3.append(pSummary)
-                }
-                // console.log(movie.title)
-                // console.log(movie.release_date)
-                
+            
+    });
+        
+        
+    select.addEventListener("change", (event) => {
+        event.preventDefault();
+        for (let movie of movies) {
+            if(select.value === movie.title) {
+                const display = document.querySelector("#display-info");
+                display.innerHTML = `<h3>${movie.title}</h3><p>${movie.release_date}</p><p>${movie.description}</p>` 
             }
-        })
-    // console.log(movie.title)
-    // console.log(movie.release_date)
-    // console.log(movie.description)
+        }
+    })
+
+        
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const li = document.createElement('li');
+        li.innerHTML = `<b>${select.value}:</b> ${input.value}`
+        const ul = document.querySelector("ul");
+        ul.append(li)
+        event.target.reset();
+    })
+    
 })
+
+
+
+    
