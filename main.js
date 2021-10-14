9017acfc8a32da616d83f19f396f10524e2908fc
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-10-14 14:09:02 -0400

    adds display functionality

--- main.js
+++ main.js
@@ -5,49 +5,57 @@ fetch(Base_URL)
     return rev.json()
 })
 .then((movies) => {
-    console.log(movies)
-    const body = document.querySelector("body");
-    const select = document.createElement("select");
-    select.setAttribute("value", "default")
-    body.append(select)
+    // const body = document.querySelector("body");
+    // const select = document.createElement("select");
+    // select.setAttribute("value", "default")
+    // body.append(select)
     
+    // section = document.createElement('section');
+    // section.setAttribute("id", "display-info")
+    // select.append(section)
+    // const form = document.createElement("form");
+    // const label = document.createElement("label");
+    // const input = document.createElement("input");
+    // const input2 = document.createElement("input")
+    // section.append(form)
+    // form.append(label)
+    // input.setAttribute("type", "text")
+    // label.append(input)
+    // input2.setAttribute("type", "submit")
+    // input2.setAttribute("value", "Submit Review")
+    // form.append(input2)
+    // const ul = document.createElement("ul")
+    // form.append(ul)
+    let select = document.querySelector("select");
+
     for (let movie of movies) {
         const option = document.createElement("option");
         option.setAttribute("value", movie.title)
         option.textContent = movie.title
         select.append(option)
-        console.log(movie.title)
+
     }
-    section = document.createElement('section');
-    section.setAttribute("id", "display-info")
-    select.append(section)
 
-
-    const form = document.createElement("form");
-    const label = document.createElement("label");
-    const input = document.createElement("input");
-    const input2 = document.createElement("input")
-    section.append(form)
-    form.append(label)
-    input.setAttribute("type", "text")
-    label.append(input)
-    input2.setAttribute("type", "submit")
-    input2.setAttribute("value", "Submit Review")
-    form.append(input2)
-
-    const ul = document.createElement("ul")
-    form.append(ul)
-// console.log(movies.release_date)
-// console.log(movies.description)
-
-
-//     option.addEventListener("onchange", () => {
-//         const h3 = document.createElement("h3")
-//         h3.textContent = movies.title
-//         const pDate = document.createElement("p")
-//         pDate.textContent = movies.release_date
-//         const pSummary = document.createElement("p")
-//         pSummary.textContent = movies.description
-//         section.append(h3)
-//     })
+        select.addEventListener("change", () => {
+            for (let movie of movies) {
+                if(select.value === movie.title) {
+                    const h3 = document.createElement("h3")
+                    h3.textContent = movie.title
+                    const pDate = document.createElement("p")
+                    pDate.textContent = movie.release_date
+                    const pSummary = document.createElement("p")
+                    pSummary.textContent = movie.description
+                    const section = document.querySelector("section");
+                    section.prepend(h3)
+                    h3.append(pDate)
+                    h3.append(pSummary)
+                }
+                // console.log(movie.title)
+                // console.log(movie.release_date)
+                
+            }
+        })
+    // console.log(movie.title)
+    // console.log(movie.release_date)
+    // console.log(movie.description)
 })
